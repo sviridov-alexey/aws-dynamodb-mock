bda2dfc3af8bd8089873e9afca0f5d33b7e3925e
Kirill <bitikit@gmail.com>
2021-10-07 22:04:43 +0300

    Create simple sql querier and dumb converter of types between SQL and DynamoDB

---
+++ src/main/kotlin/ru/hse/dynamomock/db/util/TypesConverter.kt
@@ -0,0 +1,14 @@
+package ru.hse.dynamomock.db.util
+
+object TypesConverter {
+    fun fromDynamoToSqlType(type: String): String = when (type.lowercase()) {
+        "s" -> "varchar(10000)" // TODO exclude constant
+        "bool" -> "bit"
+        "n" -> "bigint" // TODO it should be able to store any number
+        "bytebuffer" -> TODO()
+        "ss" -> TODO()
+        "ns" -> TODO()
+        "bs" -> TODO()
+        else -> type
+    }
+}
