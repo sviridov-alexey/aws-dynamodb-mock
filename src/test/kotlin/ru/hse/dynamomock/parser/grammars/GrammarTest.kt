466b9e6d1ab16957a8200c6ca1db9c3d991cf853
Kirill <bitikit@gmail.com>
2021-11-07 20:22:28 +0300

    Implement test grammar: boolean expression

---
+++ src/test/kotlin/ru/hse/dynamomock/parser/grammars/GrammarTest.kt
@@ -0,0 +1,36 @@
+package ru.hse.dynamomock.parser.grammars
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import ru.hse.dynamomock.parser.Grammar
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+internal abstract class GrammarTest<T> {
+    protected abstract val grammar: Grammar<T>
+
+    abstract fun successSource(): List<Arguments>
+
+    abstract fun failSource(): List<Arguments>
+
+    @ParameterizedTest
+    @MethodSource("successSource")
+    fun testSuccessful(expression: String, expected: T) {
+        assertEquals(expected, grammar.parse(expression))
+    }
+
+    @ParameterizedTest
+    @MethodSource("failSource")
+    fun testFailed(expression: String) {
+        assertThrows<IllegalArgumentException> {
+            grammar.parse(expression)
+        }
+    }
+
+    protected infix fun String.resulted(other: T): Arguments = Arguments.of(this, other)
+
+    protected fun failed(expression: String): Arguments = Arguments.of(expression)
+}
