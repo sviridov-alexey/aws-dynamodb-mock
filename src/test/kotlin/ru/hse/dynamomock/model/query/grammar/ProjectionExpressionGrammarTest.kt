51c8da165fefbc25d599cc671316b58e27c240cd
Kirill <bitikit@gmail.com>
2021-11-22 00:47:07 +0300

    Implement tests for projection expression

---
+++ src/test/kotlin/ru/hse/dynamomock/model/query/grammar/ProjectionExpressionGrammarTest.kt
@@ -0,0 +1,31 @@
+package ru.hse.dynamomock.model.query.grammar
+
+import ru.hse.dynamomock.model.query.QueryAttribute
+import ru.hse.dynamomock.parser.grammars.GrammarTest
+
+internal class ProjectionExpressionGrammarTest : GrammarTest<List<QueryAttribute>>() {
+    override val grammar = ProjectionExpressionGrammar(
+        mapOf(
+            "kek" to "another_kek",
+            "what" to "WhAt.that---"
+        )
+    )
+
+    private fun v(name: String) = QueryAttribute.Simple.Value(name)
+    private fun mv(attribute: QueryAttribute.Simple, value: QueryAttribute) = QueryAttribute.MapValue(attribute, value)
+    private fun lv(attribute: QueryAttribute.Simple, index: Int) = QueryAttribute.Simple.ListValue(attribute, index)
+
+    override fun successSource() = listOf(
+        "one.two.three, \n\n\t one \t\t,\t another[0].kek[1][4]" resulted listOf(
+            mv(v("one"), mv(v("two"), v("three"))), v("one"), mv(lv(v("another"), 0), lv(lv(v("kek"), 1), 4))
+        ),
+        "#kek[1][0].#kek.#what[123],o1n,kek,kek3,kek----" resulted listOf(
+            mv(lv(lv(v("another_kek"), 1), 0), mv(v("another_kek"), lv(v("WhAt.that---"), 123))),
+            v("o1n"), v("kek"), v("kek3"), v("kek----")
+        )
+    )
+
+    override fun failSource() = listOf(
+        failed("what,"), failed("#kek2,#kek,a"), failed("a,b,c["), failed(",ab,cd"), failed("a, \t, b")
+    )
+}
